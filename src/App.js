ed4e58449676d861c28f362153583fcd5166f65f
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2022-03-16 00:58:44 +0530

    Removed Footer from Explore Component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Components/About/About.js';
+import Contact from './Components/Contact/Contact.js';
+import Explore from './Components/Explore/Explore.js';
+import Gallery from './Components/Gallery/Gallery.js';
 //components
-
+import Footer from './Components/Footer/Footer.js';
 import Header from './Components/Header/Header.js';
 import Home from './Components/Home/Home.js';
-import Explore from './Components/Explore/Explore.js';
-import About from './Components/About/About.js';
-import Footer from './Components/Footer/Footer.js';
-import Contact from './Components/Contact/Contact.js';
 import JoinUs from './Components/JoinUs/JoinUs.js';
-import Gallery from './Components/Gallery/Gallery.js';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<>
 			<Header />
 			<Routes>
-				<Route path='/' element={<Home />}></Route>
+				<Route exact path='/' element={<Home />}></Route>
 				<Route path='/explore' element={<Explore />}></Route>
 				<Route path='/about' element={<About />}></Route>
 				<Route path='/contact' element={<Contact />}></Route>
 				<Route path='/join' element={<JoinUs />}></Route>
 				<Route path='/mygallery' element={<Gallery />}></Route>
 			</Routes>
-			<Footer />
-		</BrowserRouter>
+			<Routes>
+				<Route exact path='/explore' component={null}></Route>
+				<Route path='/' element={<Footer />}></Route>
+				<Route path='/mygallery' element={<Footer />}></Route>
+			</Routes>
+		</>
 	);
 }
 
